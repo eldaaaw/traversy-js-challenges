64d332e383f0b9a6a960de35a9aecf1b2858875b
Elmustafa Eldaw <eldaw@Elmustafas-MacBook-Air.local>
2023-09-14 15:17:59 +0300

    Basic

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,28 @@
-function calculator() {}
+function calculator(a, b, opreator) {
+  //   if (opreator === "+") {
+  //     return a + b;
+  //   } else if (opreator === "-") {
+  //     return a - b;
+  //   } else if (opreator === "*") {
+  //     return a * b;
+  //   } else if (opreator === "/") {
+  //     return a / b;
+  //   }
+
+  switch (opreator) {
+    case "+":
+      return a + b;
+      break;
+    case "-":
+      return a - b;
+      break;
+    case "*":
+      return a * b;
+      break;
+    case "/":
+      return a / b;
+      break;
+  }
+}
 
 module.exports = calculator;
