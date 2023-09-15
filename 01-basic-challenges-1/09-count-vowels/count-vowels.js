1a297da82feed6a230e07408ab6814d8f1013f01
Elmustafa Eldaw <eldaw@Elmustafas-MacBook-Air.local>
2023-09-16 00:50:20 +0300

    From the first time, NO FUCKIN ERRORS

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,15 @@
-function countVowels() {}
+function countVowels(str) {
+  let vouwels = ["a", "e", "i", "u", "o"];
+  let newStr = str.toLocaleLowerCase().split("");
+  let couunter = 0;
+  for (let i = 0; i < newStr.length; i++) {
+    for (let j = 0; j < vouwels.length; j++) {
+      if (newStr[i] === vouwels[j]) {
+        couunter++;
+      }
+    }
+  }
+  return couunter;
+}
 
 module.exports = countVowels;
