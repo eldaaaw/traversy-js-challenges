d60fbbfb42fd39ea5f32211d31388d2d7b91d399
Elmustafa Eldaw <eldaw@Elmustafas-MacBook-Air.local>
2023-09-16 00:41:26 +0300

    all correct without the RX

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,14 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  let newStr = str.toLocaleLowerCase().replace(/[^a-z0-9]/g, "");
+  let revStr = "";
+  for (i = newStr.length - 1; i >= 0; i--) {
+    revStr += newStr[i];
+  }
+  if (revStr === newStr) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = isPalindrome;
