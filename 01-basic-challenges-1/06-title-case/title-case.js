ba76801c08e56c430dce7c2afe49a3e2fab0bc42
Elmustafa Eldaw <eldaw@Elmustafas-MacBook-Air.local>
2023-09-16 00:01:34 +0300

    With 10% help

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,9 @@
-function titleCase() {}
+function titleCase(str) {
+  let newStr = str.toLocaleLowerCase().split(" ");
+  for (let i = 0; i < newStr.length; i++) {
+    newStr[i] = newStr[i][0].toLocaleUpperCase() + newStr[i].slice(1);
+  }
+  return newStr.join(" ");
+}
 
 module.exports = titleCase;
