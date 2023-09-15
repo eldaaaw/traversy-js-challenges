8a179dbfb79548c86c0043f2da058c402a692b0b
Elmustafa Eldaw <eldaw@Elmustafas-MacBook-Air.local>
2023-09-15 18:20:50 +0300

    Solved

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,15 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  return str.split(char).length - 1;
+}
 
 module.exports = countOccurrences;
+
+// function countOccurrences(str, char) {
+//     let counter = 0;
+//     for (let i = 0; i < str.length; i++) {
+//       if (str[i] === char) {
+//         counter++;
+//       }
+//     }
+//     return counter;
+//   }
