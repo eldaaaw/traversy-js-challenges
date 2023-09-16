f7bc7d7e5cad4ac512367a780d3b598bb8d2e5af
Elmustafa Eldaw <eldaw@Elmustafas-MacBook-Air.local>
2023-09-16 15:38:58 +0300

    second

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,10 +1,8 @@
 function arrayIntersection(arr1, arr2) {
   let arr3 = [];
   for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2.length; j++) {
-      if (arr1[i] === arr2[j]) {
-        arr3.push(arr2[j]);
-      }
+    if (arr2.includes(arr1[i]) && !arr3.includes(arr1[i])) {
+      arr3.push(arr1[i]);
     }
   }
   return arr3;
