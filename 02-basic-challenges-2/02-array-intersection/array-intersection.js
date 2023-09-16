76b885d765fbeefa96b6a101786eba1f7cca70c0
Elmustafa Eldaw <eldaw@Elmustafas-MacBook-Air.local>
2023-09-16 15:24:44 +0300

    100%

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,13 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  let arr3 = [];
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        arr3.push(arr2[j]);
+      }
+    }
+  }
+  return arr3;
+}
 
 module.exports = arrayIntersection;
