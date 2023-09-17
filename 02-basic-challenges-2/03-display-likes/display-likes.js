31de3ed4533fd0e5831908927d957c277e2f4dd7
Elmustafa Eldaw <eldaw@Elmustafas-MacBook-Air.local>
2023-09-17 15:36:33 +0300

    hard one

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,21 @@
-function displayLikes() {}
+function displayLikes(arr) {
+  switch (arr.length) {
+    case 0:
+      return "no one likes this";
+      break;
+    case 1:
+      return `${arr[0]} likes this`;
+      break;
+    case 2:
+      return `${arr[0]} and ${arr[1]} like this`;
+      break;
+    case 3:
+      return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
+      break;
+    default:
+      return `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`;
+      break;
+  }
+}
 
 module.exports = displayLikes;
--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,14 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  if (arr.length === 0) return 1;
+
+  let n = arr.length + 1;
+  let expectedSum = (n * (n + 1)) / 2;
+
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return expectedSum - sum;
+}
 
 module.exports = findMissingNumber;
