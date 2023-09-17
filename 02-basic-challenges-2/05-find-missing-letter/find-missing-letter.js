a4e512b5a96bde287699695836d7b6c2622d0c9d
Elmustafa Eldaw <eldaw@Elmustafas-MacBook-Air.local>
2023-09-17 16:52:43 +0300

    good

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,50 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  let alpha = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  let first = arr[0];
+  let NewArray = [];
+  let messing = "";
+  for (let i = alpha.indexOf(first) - 1; i <= arr.length; i++) {
+    NewArray += alpha[i];
+  }
+
+  for (let i = 0; i <= NewArray; i++) {
+    for (let j = 0; j <= arr; j++) {
+      if (arr[j] === NewArray[i]) {
+        continue;
+      } else {
+        messing = arr[j];
+      }
+    }
+  }
+  return messing;
+}
 
 module.exports = findMissingLetter;
---
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.run.js
@@ -0,0 +1,5 @@
+const findMissingLetter = require("./find-missing-letter");
+
+result = findMissingLetter(["b", "c", "d", "f", "g"]);
+
+console.log(result);
