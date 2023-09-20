5d2f5cda445e3bdc69f0c7af33a73d8686980a4a
Elmustafa Eldaw <eldaw@Elmustafas-MacBook-Air.local>
2023-09-20 22:35:36 +0300

    Too easy

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,8 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(array) {
+  for (let i = 0; i < array.length; i++) {
+    const phone = `(${array[0]}${array[1]}${array[2]}) ${array[3]}${array[4]}${array[5]}-${array[6]}${array[7]}${array[8]}${array[9]}`;
+    return phone;
+  }
+}
 
 module.exports = formatPhoneNumber;
